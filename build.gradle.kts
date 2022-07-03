f590e00519d09dd30167cd65814dc3d653500719
Igor Stepin <stepin@stepin.name>
2022-07-04 01:32:32 +0400

    More done

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
     application
+    id("info.solidsoft.pitest")
 }
 
 group = "name.stepin"
@@ -10,9 +11,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/stepin/pitest-kotlin")
+        credentials {
+            username = System.getenv("GITHUB_ACTOR")
+            password = System.getenv("GITHUB_TOKEN")
+        }
+    }
 }
 
 dependencies {
+    compileOnly("name.stepin:pitest-kotlin-plugin:0.1.0")
     testImplementation(kotlin("test"))
 }
 
@@ -26,4 +35,8 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.build {
+    dependsOn(tasks.pitest)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
-
 rootProject.name = "pitest-kotlin-sample"
 
+pluginManagement {
+    val pitestVersion: String by settings
+    plugins {
+        id("info.solidsoft.pitest") version "$pitestVersion"
+    }
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/stepin/pitest-kotlin")
+        }
+    }
+}
